4298b54f18d089343954a8a1b126a9bc4f232c22
suddicus <suhdenzo04@gmail.com>
2024-06-09 15:17:30 +0100

    work on game activity and setting up activity for game

--- app/src/main/java/com/jhin/pong2dgame/GameActivity.java
+++ app/src/main/java/com/jhin/pong2dgame/GameActivity.java
@@ -1,6 +1,8 @@
 package com.jhin.pong2dgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class GameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(GameActivity.this,PongActivity.class);
+        startActivity(intent);
     }
 }
